1dd4bc5d8f65aca7720719545074ff573727872d
Phil Dawson <37028597+pDawson17@users.noreply.github.com>
2020-11-04 15:15:29 -0800

    light/dark mode cookie created

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+
+@app.route('/set-background/<mode>')
+def set_background(mode):
+    session['mode'] = mode
+    return render_template('home.html')# redirect(url_for('/'))
 
 @app.route('/', methods=['GET'])
 def my_route_page_function():
